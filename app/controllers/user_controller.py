187b874017a8f5bb0af65db3af387a6950c607e0
rdp-jr <rdprincipio.jr@gmail.com>
2021-11-19 00:47:34 +0800

    fix: add redirect to dashboard from register and login page when logged in

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for
 from app.models.user import User
 from app.forms.user.LoginForm import LoginForm
 from app.forms.user.RegisterForm import RegisterForm
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from app import flask_bcrypt
 
 
@@ -79,6 +79,10 @@ def logout():
 
 
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('users.dashboard'))
+
+
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -91,6 +95,11 @@ def login():
     return render_template('user/login.html', form=form)
 
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('users.dashboard'))
+
+
+
     form = RegisterForm()
 
     if form.validate_on_submit() and form.password.data == form.confirm_password.data:
